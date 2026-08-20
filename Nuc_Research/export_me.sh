dad4387edfdaf1dc79754f12270c5a868c492e9c
onippejeakins <118579301+onippejeakins@users.noreply.github.com>
2026-08-20 12:44:46 -0500

    Add files via upload

---
+++ Nuc_Research/export_me.sh
@@ -0,0 +1,10 @@
+exp=./ExportTH/export_th1d
+$exp phi_nonsrc_o_enhanced_para.txt phi_nonsrc_omega_enhanced outtiff_opp.root
+$exp phi_src_o_enhanced_para.txt phi_src_omega_enhanced outtiff_opp.root
+$exp phi_nonsrc_o_enhanced_perp.txt phi_nonsrc_omega_enhanced outtiff.root
+$exp phi_src_o_enhanced_perp.txt phi_src_omega_enhanced outtiff.root
+
+
+
+
+
